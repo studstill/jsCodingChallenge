4281ce313b89a1a346cd6265dd15d963cc01c6ab
Jason Studstill <standstill64@gmail.com>
2015-06-07 18:04:08 -0700

    Define constructers for Library, Shelf, and Book in the same JS file

--- modelLibrary/model.js
+++ modelLibrary/model.js
@@ -2,63 +2,59 @@
 Model a library using objects. *
 A library contains shelves. Shelves contain books. Books have an author and a title. Give the library methods to add and remove a shelf. A shelf should have methods to add/remove a book. Use a modular approach so that each type of object is contained in its own file. Add any functionality you think appropriate.
 */
-// 1 Create a Library constructor
-
-function Library() {
-  // 1.1 Property: Shelf
-  this.shelves = [];
-  // 1.2 Method: Add Shelf
-  this.addShelf = function(shelf) {
-    if (typeof(shelf)) === "object") {
-      this.shelves.push(shelf);
+function Library(name) {
+  this.libraryName = name;
+  this.addShelf = function(genre) {
+    // Handle if a pre-existing Shelf object is passed in as
+    // an argument
+    if (typeof(genre) === "object") {
+      var shelfGenreName = genre.genre;
+      this[shelfGenreName] = new Shelf(shelfGenreName);
     } else {
-      // Create a new shelf with shelf argument converted to "genre" property.
-      this.shelves.push(new Shelf(shelf));
+      this[genre] = new Shelf(genre);
     }
   }
-  // **** TODO ****
-  // 1.3 Method: Remove Shelf
-  this.removeShelf = function(shelf) {
-
-    var shelfIndex = this.shelves.indexOf(genreShelf);
-      this.shelves[shelfIndex].splice(0, this.shelves[shelfIndex].length)
+  this.removeShelf = function(genre) {
+    // Handle if a pre-existing Shelf object is passed in as
+    // an argument
+    if (typeof(genre) === "object") {
+      var shelfGenreName = genre.genre;
+      delete this[shelfGenreName];
+    } else {
+      delete this[genre];
+    }
   }
 }
-
-
-
 
 // 1.1 Create Shelf constructor
 function Shelf(genre) {
   this.genre = genre;
-  this.books = [];
-  //1.1 Method: Add Book
-  this.addBook = function(book) {
-    this.books.push(book);
+  this.addBook = function(title, author) {
+    // Handle if a pre-existing Book object is passed in as
+    // an argument
+    if (typeof(title) === "object") {
+      var bookObject = title;
+      var bookTitle = bookObject.title;
+      var bookAuthor = bookObject.author;
+      this[bookTitle] = new Book(bookTitle, bookAuthor);
+    } else {
+      this[title] = new Book(title, author);
+    }
   }
-  // *** TODO ****
-  //1.1.1 Method: Remove Book
-  this.removeBook = function(author, title) {
-    removedAuthor = author;
-    removedTitle = title;
-    this.books = this.books.map(function (currentValue, index) {
-      if (removedTitle === currentValue.title && removedAuthor === currentValue.author) {
-        this.books.splice(index, 1);
-        console.log("We got in here");
-      }
-    });
+  this.removeBook = function(title, author) {
+    // Handle if a pre-existing Book object is passed in as
+    // an argument
+    if (typeof(title) === "object") {
+      var bookTitle = title.title;
+      delete this[bookTitle];
+    } else {
+      delete this[title];
+    }
   }
 }
-
-
 
 // 1. Create Book constructor
-function Book(author, title) {
-  // 1.1 Author
-  this.author = author;
-  // 1.2 Title
+function Book(title, author) {
   this.title = title;
+  this.author = author;
 }
-
-  // 1.3
-//
