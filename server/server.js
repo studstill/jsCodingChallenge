d889abe7c41228a463321876bf05660824b0e9b5
Jason Studstill <standstill64@gmail.com>
2015-06-16 10:40:35 -0700

    Edit for style

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 /*
 Create a simple http server using node. *
-This server should respond to a root-url request with a file called index.html. Do not use ExpressJS. Your code should have error checking and at least one callback. Put five or more html elements in your index.html. One of the elements should be a link to an external page. Things to research: node.js, callbacks, the fs module, the http module.
+This server should respond to a root-url request with a file called
+index.html. Do not use ExpressJS. Your code should have error checking
+and at least one callback. Put five or more html elements in your
+index.html. One of the elements should be a link to an external page.
+Things to research: node.js, callbacks, the fs module, the http module.
 */
 
 // Create a web server
@@ -8,10 +12,10 @@ var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 3000;
 
-var server = http.createServer(function (request, response) {
-    fs.readFile("./index.html", function(error, data) {
+var server = http.createServer(function(request, response) {
+    fs.readFile('./index.html', function(error, data) {
       if (error) throw error;
-      response.writeHead(200, {'Content-type': 'text/html', "Conten-Length": data.length});
+      response.writeHead(200, {'Content-type': 'text/html', 'Content-Length': data.length});
       response.write(data);
       response.end();
     });
@@ -19,3 +23,6 @@ var server = http.createServer(function (request, response) {
 
 server.listen(port);
 console.log('Server running on port ' + port);
+
+
+
