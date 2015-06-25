0e21fd504a63358e32ddd87bd1b4b881b25cc5be
Jason Studstill <standstill64@gmail.com>
2015-06-24 19:25:26 -0700

    Create requestHangler callback function.  Explicitly handle errors if file index.html is not found or if any other path besides the root-url is attempted.

--- server/server.js
+++ server/server.js
@@ -11,18 +11,27 @@ Things to research: node.js, callbacks, the fs module, the http module.
 var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 3000;
-
-var server = http.createServer(function(request, response) {
-    fs.readFile('./index.html', function(error, data) {
-      if (error) throw error;
-      response.writeHead(200, {'Content-type': 'text/html', 'Content-Length': data.length});
-      response.write(data);
-      response.end();
-    });
-  });
+var server = http.createServer();
 
 server.listen(port);
 console.log('Server running on port ' + port);
 
+server.on('request', requestHandler);
 
+function requestHandler(request, response) {
+  if (request.url === '/') {
+    fs.readFile('./index.html', function(error, data) {
+    if (error) {
+      response.write('There was an error accessing index.html - ' + error);
+    } else {
+      response.writeHead(200, {'Content-type': 'text/html'});
+      response.write(data);
+    }
+    response.end();
+    });
+  } else {
+  response.writeHead(404, {'Content-Type': 'text/html'});
+  response.end('<h1>Sorry, the page you are looking for cannot be found.</h1>');
+  }
+}
 
